087e2deb047f56b95302a66de81e3f6222f21885
Hamood Thariq <143422757+hxm-xd@users.noreply.github.com>
2025-09-28 02:52:25 +0530

    Structured Folder

---
+++ src/main/java/com/example/pdsacw/PdsaCwApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdsacw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsaCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsaCwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pdsacw/PdsaCwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdsacw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsaCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
