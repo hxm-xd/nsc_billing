9f3c9a52ad8c1e4a9ed852b0bc58e3c51d0896ad
Hamood Thariq <143422757+hxm-xd@users.noreply.github.com>
2025-10-17 04:33:13 +0530

    Minor improvements to UI

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 600){
+            header.classList.add('h-after');
+        }else{
+            header.classList.remove('h-after');
+        }
+    })
+})
